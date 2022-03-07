f80f58ab0f517b699f4ebdc594540f358fcbd805
Asad Alam <asad.alam4589@gmail.com>
2022-03-08 01:54:04 +0530

    used anime-api

--- script.js
+++ script.js
@@ -1,33 +1,41 @@
+const API_URL = 'https://animechan.vercel.app/api/quotes'
+
 const app = document.getElementById('root');
-
-const logo = document.createElement('img');
-
-logo.src = './images/logo.png';
 
 const container = document.createElement('div');
 container.setAttribute('class', 'container');
 
-app.appendChild(logo);
 app.appendChild(container);
 
 const getData = async() => {
-    const response = await fetch('https://ghibliapi.herokuapp.com/films')
+    const response = await fetch(API_URL)
     const data = await response.json()
-
+    console.log(data)
     data.forEach(movie => {
         const card = document.createElement('div');
         card.setAttribute('class', 'card')
     
         const h1 = document.createElement('h1')
-        h1.textContent = movie.title
-    
-        const p = document.createElement('p')
-        description = movie.description.substring(0, 300)
-        p.textContent = `${description}...`
-    
+        h1.textContent = movie.anime;
+        h1.setAttribute('class', 'title')
+        
+        const blockQuote = document.createElement('div');
+        blockQuote.setAttribute('class', 'blockquote')
+        const quote = document.createElement('div')
+        description = movie.quote;
+        quote.textContent = `${description}`
+        quote.setAttribute('class', 'quote')
+
+        const credit = document.createElement('div');
+        credit.textContent = movie.character;
+        credit.setAttribute('class', 'credit');
+        
+        blockQuote.appendChild(quote)
+        blockQuote.appendChild(credit)
+
         container.appendChild(card)
         card.appendChild(h1)
-        card.appendChild(p);
+        card.appendChild(blockQuote);
     })
 
 }
